d8406f182d8040c96f7bc86af81f4e2de6796b85
Juan Velandia <juanvelandia@protonmail.com>
2020-09-07 17:36:42 -0500

    Move logic to Instrument

--- POCs/Audio/instrument.js
+++ POCs/Audio/instrument.js
@@ -1,7 +1,42 @@
 class Instrument{
-  constructor(audioContext){
+  constructor(audioContext, numberOfOscillators = 1){
     this.context = audioContext;
+    this.numberOfOscillators = numberOfOscillators;
   }
+  createOscillators() {
+    let oscillators = [];
+    let gains = [];
+    for(let i = 0; i < this.numberOfOscillators; i++){
+      let o = this.context.createOscillator();
+      let g = this.context.createGain();
+      o.connect(g);
+      g.connect(this.context.destination);
+      oscillators.push(o);
+      gains.push(g);
+    }
+    return [oscillators, gains];
+  }
+
+  setFrequencies(oscillators, freqArray) {
+    freqArray.forEach((f,i) => {oscillators[i].frequency.value = f});
+  }
+
+  setGains(gains, gainArray) {
+    gainArray.forEach((f,i) => {gains[i].gain.value = f});
+  }
+
+  setExponentialRampToValueAtTime(gains, time){
+    gains.forEach(g => {g.gain.exponentialRampToValueAtTime(0.1,time)});
+  }
+
+  start(oscillators, time){
+    oscillators.forEach(o => {o.start(time)})
+  }
+
+  stop(oscillators, time){
+    oscillators.forEach(o => {o.stop(time)})
+  }
+
   playNote(note, figure){
   }
 }
--- POCs/Audio/piano.js
+++ POCs/Audio/piano.js
@@ -1,61 +1,26 @@
 class Piano extends Instrument {
-  playNote(noteFreq, time){
-    // creates oscillator to create the actual sound with the choosen note frequency
-    let o = this.context.createOscillator()
-    let o2 = this.context.createOscillator()
-    let o3 = this.context.createOscillator()
-    let o4 = this.context.createOscillator()
-    let o5 = this.context.createOscillator()
-    o.frequency.value = noteFreq;
-    o2.frequency.value = noteFreq + (Math.random() * 10) - 5;
-    o3.frequency.value = noteFreq + (Math.random() * 15) - 5;
-    o4.frequency.value = noteFreq / 2;
-    o5.frequency.value = noteFreq / 4;
-
-
-    let g = this.context.createGain() // creates gain to gracefully reduce the sound through time
-    let g2 = this.context.createGain() 
-    let g3 = this.context.createGain() 
-    let g4 = this.context.createGain() 
-    let g5 = this.context.createGain() 
-
-    g.gain.value = 0.2
-    g2.gain.value = 0.15
-    g3.gain.value = 0.12
-    g4.gain.value = 0.1
-    g5.gain.value = 0.05
-
-    o.connect(g) //Connects oscillator output to gain input
-    o2.connect(g2);
-    o3.connect(g3);
-    o4.connect(g4);
-    o5.connect(g5);
-
-    g.connect(this.context.destination) //Conect gain module to the output
-    g2.connect(this.context.destination) //Conect gain module to the output
-    g3.connect(this.context.destination) //Conect gain module to the output
-    g4.connect(this.context.destination) //Conect gain module to the output
-    g5.connect(this.context.destination) //Conect gain module to the output
-
-    o.start(this.context.currentTime + time)//Start playing
-    o2.start(this.context.currentTime + time) //Start playing
-    o3.start(this.context.currentTime + time) //Start playing
-    o4.start(this.context.currentTime + time) //Start playing
-    o5.start(this.context.currentTime + time) //Start playing
-
-    // Reduces exponentially the sound, this gives a reallystic feeling
-    // as the produced sound decreases the same way the sound of a pulled
-    // guitar string would decrease.
-    g.gain.exponentialRampToValueAtTime(0.1, this.context.currentTime + time + 1.25)
-    g2.gain.exponentialRampToValueAtTime(0.1, this.context.currentTime + time + 1.25)
-    g3.gain.exponentialRampToValueAtTime(0.1, this.context.currentTime + time + 1.25)
-    g4.gain.exponentialRampToValueAtTime(0.1, this.context.currentTime + time + 1.25)
-    g5.gain.exponentialRampToValueAtTime(0.1, this.context.currentTime + time + 1.25)
-
-    o.stop(this.context.currentTime + time + 1.5)
-    o2.stop(this.context.currentTime + time + 1.5)
-    o3.stop(this.context.currentTime + time + 1.5)
-    o4.stop(this.context.currentTime + time + 1.5)
-    o5.stop(this.context.currentTime + time + 1.5)
+  constructor(audioContext){
+    super(audioContext, 5);
+  }
+  playNote(noteFreq, noteTime, decTime){
+    let [oscillators, gains] = this.createOscillators();
+    this.setFrequencies(oscillators, [
+      noteFreq,
+      noteFreq + (Math.random() * 10) - 5,
+      noteFreq + (Math.random() * 15) - 5,
+      noteFreq / 2,
+      noteFreq / 4
+    ]);
+    this.setGains(gains, [
+      0.2,
+      0.15,
+      0.12,
+      0.1,
+      0.05
+    ]);
+    this.start(oscillators, noteTime)
+    this.setExponentialRampToValueAtTime(gains, this.context.currentTime + noteTime + decTime)
+    let stopDelay = decTime + 0.5;
+    this.stop(oscillators, this.context.currentTime + noteTime + stopDelay);
   }
 }
