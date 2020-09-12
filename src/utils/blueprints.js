81d9e1ef3a2ac1e24908e2632da0ee4667f18fde
Juan Velandia <juankz@protonmail.com>
2020-09-12 18:54:10 -0500

    Add more levels and story

--- src/utils/blueprints.js
+++ src/utils/blueprints.js
@@ -1,3 +1,5 @@
+import * as terminalData from './terminal.js';
+
 const BEHAVIOURS = {
   TRANSLATION: 0,
   ROTATION: 1,
@@ -13,6 +15,9 @@ g: goal
 */
 
 const blueprints = [
+
+  // Level 0
+
   {
     level:
     `
@@ -26,40 +31,63 @@ const blueprints = [
     xxxxxxxx
     
     `,
-    terminal: 'tutorial'
+    terminals: [
+      {
+        tutorial: true
+      }
+    ]
   },
+
+  // Level 1
+
   {
     level:
     `
-    xxxxxxxxxxxxxxxxx
-    x----x----------x
-    dp--Tx----------x
-    x----x--xx--xx--x
-    x---------------x
-    x--------------gd
-    xxxxx---xxx-----x
-    ---x----------xxx
+    xxxxxxxxxxxxxxxxxxxx
+    x----x-------------x
+    dp--Tx------------tx
+    x----x--xx--xxx----xxxx
+    x---------------------x
+    x--------------------gd
+    xxxxx---xxx--xxxxxxxxxx
+    ---x----------x
     ---x----------x
     ---x-0--------x
     ---xxxxxxxxxxxx
     `,
-    terminal: `Don't let them see you!`,
+    terminals: [
+      {
+        text: `
+Find Ned Snow!
+Search for more terminals
+But don't let them see you!
+`,
+
+      },
+      {
+        text: terminalData.unencryptedData[0],
+      }
+    ],
     robots: [
       {
         behaviour: BEHAVIOURS.TRANSLATION,
         target: new THREE.Vector3(7, 0, 0),
         await: 5000,
         rotationY: Math.PI,
-        delay: 4000
+        delay: 6000
       }
     ]
   },
+
+  // Level 2
+
   {
     level:
     `
-    xxxxxxxxxxxxxxx
-    x-------------x
-    dp------------x
+    xxxxx
+    x---x
+    dp-Tx
+    x---xxxxxxxxxxx
     x-------------x
     x------------gd
     x--xoooooooxxxx
@@ -68,6 +96,9 @@ const blueprints = [
     x--x---0------x
     xxxxxxxxxxxxxxx
     `,
+    terminals: [
+      {text: 'Crouch'}
+    ],
     robots: [
       {
         rotationY: Math.PI
@@ -76,6 +107,16 @@ const blueprints = [
       }
     ]
   },
+
+  // Level 3
+
+  {
+    level: terminalData.blueprint,
+    terminals: [{text: terminalData.text_404}]
+  },
+
+  // Level 4
+
   {
     level:
      `
@@ -91,13 +132,19 @@ const blueprints = [
      x-----o---0--x---gd
      x-----o------x----x
      x-----------------x
-     x--------ooo------x
-     x-----------------x
-     x-----------------x
-     x-----------------x
+     x---x----ooo------x
+     x---x-------------x
+     x---x-------------x
+     x-$-x-------------x
      xxxxxxxxxxxxxxxxxxx
 
      `,
+     terminals: [
+       {
+         text: terminalData.encryptedData[0],
+         rotation: Math.PI
+       }
+     ],
      robots: [
        {
          behaviour: BEHAVIOURS.ROTATION,
@@ -108,21 +155,217 @@ const blueprints = [
        }
      ]
   },
+
+  // Level 5
+
+  {
+    level: // 5
+    `
+    xxxxxxxxxxxxxxxxxxx
+    x---o--o----------x
+    dp--o--o----------x
+    x---oooo--oooo----x
+    x------------o----x
+    x------------o----x
+    xxxxooooooo--o----x
+    x---o--o--o--o----x
+    x---oooo--oooo----x
+    x-----------------x
+    x-----------------x
+    x---ooo---ooooxxoxx
+    x---o-------------x
+    x---o-------------x
+    x---o---0---------x
+    x---o-------------x
+    x-t-o-------------x
+    xxxxxxx---oooooo-ox
+    ------x-----------x
+    ------x---------g-x
+    ------xxxxxxxxxxdxx
+    `,
+    terminals: [
+      {
+        text: terminalData.unencryptedData[1],
+        rotation: Math.PI
+      }
+    ],
+    robots: [
+      {
+        behaviour: BEHAVIOURS.TRANSLATION,
+        target: new THREE.Vector3(7, 0, 0),
+        await: 5000,
+        rotationY: Math.PI,
+        delay: 4000
+      }
+    ]
+  },
+
+  // Level 6
+  {
+    level:
+      `
+      ------xxxxxxxxxxxxxx
+      ------x------x--t--x
+      ------x------x-----x
+      xxxxxxx------x-----x
+      x------------------x
+      x$-----------------x
+      x-----x------x-----x
+      x-----x------x-----x
+      xxxxxxxxx--xxxxxxxx
+      x-----------------x
+      x----------------gd
+      x----------------x
+      x---xx-W------xxxx
+      x---o----------x
+      x---o----------x
+      dp--o----------x
+      x---o----0-----x
+      xxxxxxxxxxxxxxxx
+      `,
+      terminals: [
+        {
+          text: terminalData.unencryptedData[2],
+          rotation: Math.PI*2
+        },
+        {
+          text: terminalData.encryptedData[1],
+          rotation: Math.PI/2
+        },
+      ],
+      movingWalls: [
+        {
+          target: new THREE.Vector3(5, 0, 0),
+          await: 5000,
+          delay: 3000,
+          speed: 1,
+        }
+      ],
+      robots: [
+        {
+          rotationY: Math.PI
+        },
+      ]
+    
+  },
+
+  // Level 7
+
+  {
+    level: terminalData.blueprint,
+    terminals: [{text: terminalData.text_404}]
+  },
+  // Level 8
+  {
+    level: `
+    -------------------xxdxx
+    xxxxxxxxxxxxxxxxxxxx-g-x
+    x------------------x---x
+    x------------------x---x
+    x----x----o--------x---x
+    x----o---------o-------x
+    x----o---------o---x---x
+    dp--------0--------x---x
+    x----------------------x
+    x----o---------o-------x
+    x------------------x---x
+    xx--xxx-xx-xx-xxxxxxxxxx
+    x-------------------x
+    x------------------tx
+    x-------------------x
+    xxxxxxxxxxxxxxxxxxxxx
+    `,
+    terminals: [
+      {text: terminalData.unencryptedData[3]}
+    ],
+    robots: [
+      {
+        behaviour: BEHAVIOURS.ROTATION,
+        targets: [0],
+        speed: -Math.PI/4,
+        delay: 0,
+        await: 0,
+        rotationY: Math.PI
+      }
+    ]
+  },
+
+  // Level 9
   {
     level:
     `
-    xxxxxxx
-    x-----x
-    dp---Tx
-    x-----x
-    x------x
-    x------x
-    x------x
-    xxxxxxxx
-    
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    x--------------------------x
+    dp------------------------$x
+    xooooooo---xx--------------x
+    x------o--------xxxoo---xxxx
+    x------o------------o---x
+    x------oo-----------o---x
+    xxxxx-W-----------------x
+    ----x-------------W-----x
+    ----x-------------------x
+    ----x-------------------x
+    ----dg------------------x
+    ----x------ooooooxxxxxxxx
+    ----x------o---x
+    ----x------o-0-x
+    ----x------o---x
+    ----xxxxxxxxxxxx
     `,
-    terminal: 'The End'
+    terminals: [
+      {
+        text: terminalData.encryptedData[2],
+      }
+    ],
+    movingWalls: [
+      {
+        target: new THREE.Vector3(10, 0, 0),
+        await: 0,
+        delay: 0,
+        speed: 2.5,
+      },
+
+      {
+        target: new THREE.Vector3(-10, 0, 0),
+        await: 0,
+        delay: 0,
+        speed: 2.5,
+      },
+
+
+      {
+        target: new THREE.Vector3(8, 0, 0),
+        await: 0,
+        delay: 0,
+        speed: 3,
+      }
+    ],
+    robots:[{
+      rotationY: Math.PI
+    }]
   },
+  // Level 10
+
+  {
+    level: terminalData.blueprint,
+    terminals: [{text: terminalData.text_200}]
+  },
+  
+  // Level 11
+  {
+    level: `
+    xxxxxx
+    x----x
+    dp--Tx
+    x----x
+    xxxxxx
+    `,
+     terminals: [
+       {text: terminalData.end}
+     ]
+  }
+  // Level 12
+
 ];
 
 export {BEHAVIOURS, blueprints};
--- src/utils/terminal.js
+++ src/utils/terminal.js
@@ -1,15 +1,13 @@
 const blueprint = `
 xxxxxxxxxxxxx
 x-----------x
-x--|--|--|--x
 x-----------x
-x--|--|--|--x
+x-|-|-|-|-|-x
 x-----------x
 dp---------tx
 x-----------x
-x--|--|-----x
+x-|-|-|-|---x
 x----------gd
-x--|--|-----x
 x-----------x
 xxxxxxxxxxxxx
 
@@ -40,8 +38,34 @@ your controller and press
 the trigger to move. Can
 you reach the door? ->`;
 
-const text_404 = `Searching Ned Snow
+const text_404 = `Searching for Ned Snow
+.......................
+.......................
+.......................
+.......................
+.......................
+
 404 Person not found
+`
+
+const text_200 =  `
+Searching for Ned Snow
+.......................
+.......................
+200 found Ned Snow.
+Initiating extraction
+.......................
+.......................
+Extraction successful
+Ned Snow has been liberated.
+`
+
+const end =  `
+Congratulations!
+
+You made it to the end.
+
+Thanks for playing.
 `
 
 const unencryptedData = [
@@ -49,15 +73,14 @@ const unencryptedData = [
 
 You are on the facility 502.
 This instalation is reserved for
-dangerous individuals according
-to Law 409.
+individuals who violated Law 409.
 `,
 `LAW 409
 Any individual who opposes power
 institutions by "making a stand",
 "speak up", or any other form of
 manifestation will be put on 
-aritificial coma for an undefined
+induced coma for an undefined
 period.
 `,
 `NED SNOW PROFILE
@@ -65,6 +88,11 @@ period.
 Activist. Ned Snow used to advocate
 for privacy and opposed to mass
 surveillance.
+
+He fought fiercely agains law 409.
+`,
+`
+
 `
 ]
 
@@ -75,29 +103,39 @@ After it was evident that humans were unrealible
 when holding power we tried to move slowly towards
 a technocracy.
 
-It seemed like a really good idea, an unbiased
-machine aiding government to take decisions.
+It seemed like a good idea, an unbiased AI
+aiding the government to take decisions.
 But machines suffered from the same sins 
-of their creators. Soon we had a techtyranny.
+of their creators. Soon we had a tech-tyranny.
 `,
 `Notes from the diary of subject 406. 2/5
 INTERFACING OUR BRAINS WITH MACHINES
-It was like a miracle. We were able to cure alzhaimer and other brain problems.
-We should have stopped there.
+It was like a miracle. We were able to cure alzhaimer
+and other brain problems.
+We should have stopped there. But we opened our minds
+And when law 409 went to action, even our thoughts
+made us criminals.
 `,
-`Notes from the diary of subject 406. 3/5
-Previously the crazy drunk person were telling stupid things at the bar,
-everyone could ignore him.
-Then internet came, the crazy drunk found other crazy drunks, they craeted their own cult
-full of conspiracy theories. Soon they became too dangerous to be ignored.
+`Notes from the diary of subject 406. 4/5
+"If you want a picture of the future, imagine 
+a boot stamping on a human face, forever.
+The moral to be drawn from this dangerous nightmare
+situation is a simple one: 
+Don't let it happen. It depends on you."
+
+-George Orwell about his book 1984.
 `,
-`Notes from the diary of subject 406. 5/5
-George Orwell wrote 1984. The most terrifying book in history. He left us a warning:
-"If you want a picture of the future, imagine a boot stamping on a human face, forever.
-  The moral to be drawn from this dangerous nightmare situation is a simple one: 
-  don't let it happen. It depends on you."
-  I won't let it happen!
+
+`
+Access to facility 502 granted.
+.........................
+.........................
+Liberating all prisioners
+.........................
+.........................
+.........................
+All prisioners are free now.
 `,
 ]
 
-export {blueprint, tutorial_VR, tutorial_desktop, tutorial_mobile, text_404, unencryptedData, encryptedData}
+export {blueprint, tutorial_VR, tutorial_desktop, tutorial_mobile, text_404, text_200, unencryptedData, encryptedData, end}
