bf015101b6c40337a4d6e9a00164738a735c71b0
Juan Velandia <juankz@protonmail.com>
2020-09-12 18:49:56 -0500

    Add human shape

--- POCs/canvas_image/index.js
+++ POCs/canvas_image/index.js
@@ -5,10 +5,61 @@ canvas.width = 512;
 canvas.height = 512;
     
 const ctx = canvas.getContext('2d');
-drawWall(ctx);
+drawHuman(ctx);
+// drawWall(ctx);
 // drawCircularGradient(ctx)
 // drawText(ctx, text)
 // drawFloor(ctx);
+
+function drawHuman(ctx){
+  let w = ctx.canvas.width;
+  let h = ctx.canvas.height
+  
+  var grd = ctx.createLinearGradient(0, 0, 0, h);
+  grd.addColorStop(0, 'green');
+  grd.addColorStop(1, 'red');
+  ctx.fillStyle = '#gray';
+  ctx.fillRect(0, 0, w, h);
+  ctx.strokeStyle = grd;
+  ctx.lineWidth = 6 ;
+  ctx.beginPath();
+  ctx.moveTo(250, 250);
+  ctx.lineTo(245, 420);
+  ctx.lineTo(200, 420);
+  ctx.lineTo(200, 150);
+  ctx.lineTo(190, 280);
+  ctx.lineTo(160, 280);
+  ctx.lineTo(170, 110);
+  ctx.lineTo(200, 90);
+  ctx.lineTo(250, 90);
+  ctx.stroke();
+
+  ctx.moveTo(250, 250);
+  ctx.lineTo(255, 420);
+  ctx.lineTo(300, 420);
+  ctx.lineTo(300, 150);
+  ctx.lineTo(310, 280);
+  ctx.lineTo(340, 280);
+  ctx.lineTo(330, 110);
+  ctx.lineTo(300, 90);
+  ctx.lineTo(250, 90);
+  ctx.stroke();
+
+  ctx.moveTo(0, 0);
+  ctx.strokeRect(225, 20, 50, 50);
+
+
+  // ctx.lineTo(200, 400);
+  // ctx.lineTo(200, 150);
+  // ctx.lineTo(190, 280);
+  // ctx.lineTo(160, 280);
+  // ctx.lineTo(170, 100);
+  // ctx.lineTo(250, 100);
+  // ctx.lineTo(300, 150);
+  // ctx.lineTo(300, 400);
+  // ctx.lineTo(260, 400);
+  // ctx.lineTo(250, 250);
+}
 
 function drawWall(ctx){
   let w = ctx.canvas.width;
