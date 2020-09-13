b5d75363a5f3bf44d5edefd34b469c6e2fdc182c
Juan Velandia <juankz@protonmail.com>
2020-09-13 00:08:06 -0500

    Finish terminals story

--- src/utils/terminal.js
+++ src/utils/terminal.js
@@ -17,7 +17,7 @@ const tutorial_mobile =
 `Instructions:
 Stand in front of a terminal
 to interact with it.
-
+Turn in real life.
 Point at the floor and tap
 Tap & hold to crouch/stand.
 Can you reach the door? ->`;
@@ -25,7 +25,7 @@ const tutorial_desktop =
 `Instructions:
 Stand in front of a terminal
 to interact with it.
-
+Use you mouse to turn,
 Move with the keys W A S D,
 SPACE to crouch/stand.
 Can you reach the door? ->`;
@@ -88,7 +88,7 @@ He fought fiercely agains law 409.
 ]
 
 const encryptedData = [
-`Notes from the diary of subject 406. 1/5
+`Notes from the diary of subject 406. 1/3
 TRANSITION TO TECHNOCRACY.
 After it was evident that humans were unrealible
 when holding power we tried to move slowly towards
@@ -99,7 +99,7 @@ aiding the government to take decisions.
 But machines suffered from the same sins 
 of their creators. Soon we had a tech-tyranny.
 `,
-`Notes from the diary of subject 406. 2/5
+`Notes from the diary of subject 406. 2/3
 INTERFACING OUR BRAINS WITH MACHINES
 It was like a miracle. We were able to cure alzhaimer
 and other brain problems.
@@ -107,7 +107,7 @@ We should have stopped there. But we opened our minds
 And when law 409 went to action, even our thoughts
 made us criminals.
 `,
-`Notes from the diary of subject 406. 4/5
+`Notes from the diary of subject 406. 3/3
 "If you want a picture of the future, imagine 
 a boot stamping on a human face, forever.
 The moral to be drawn from this dangerous nightmare
