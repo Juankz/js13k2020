856868f882806609b37eab46f062f65774281e67
Juan Velandia <juankz@protonmail.com>
2020-09-13 00:09:07 -0500

    Fix typo

--- src/systems/sound.js
+++ src/systems/sound.js
@@ -13,7 +13,7 @@ const FREQUENCIES = {
   'C1': 261.63, 'C#1': 277.18, 'D1': 293.66, 'D#1': 311.13,
   'E1': 329.63, 'F1': 349.23, 'F#1': 369.99, 'G1': 391.99,
   'G#1': 415.30, 'A1': 440.00, 'A#1': 466.16, 'B1': 493.88,
-  'C2' : 523.25, 'C2#': 554.36, 'D2': 587.33, 'D#2' : 622.25,
+  'C2' : 523.25, 'C#2': 554.36, 'D2': 587.33, 'D#2' : 622.25,
   'E2' : 659.25, 'F2' : 698.45, 'F#2' : 739.99, 'G2': 783.99,
   'G#2' : 839.61, 'A2' : 880.00, 'A#2' : 932.33, 'B2' : 987.76,
   'B2' : 987.76, 'C3' : 1046.50, 'C#3' : 1108.73, 'D3' : 1174.66,
@@ -34,7 +34,7 @@ export default AFRAME.registerSystem('gameaudio', {
       let notes = note.split(',');
       notes.forEach(n => {
         if (note !== '-'){
-          let dec = NOTE_TYPE[decTime] * 60 / tempo;
+          let dec = NOTE_TYPE[decTime] * 120 / tempo;
           instrument.playNote(FREQUENCIES[n], time, dec, volume);
         }
       });
