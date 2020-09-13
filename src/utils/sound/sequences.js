68ccfc28c20a1efcb71ca68e37041e7bdbf88ffa
Juan Velandia <juankz@protonmail.com>
2020-09-13 00:07:48 -0500

    Better background music

--- src/utils/sound/sequences.js
+++ src/utils/sound/sequences.js
@@ -3,45 +3,22 @@ const Sequences = {
     'C#1,D#1,A1 w s'
   ],
 
-  background: [
-    'C1 w s', 'D1 w s',
-    'C1 w s', 'D1 w s',
+  vertigo: [
+    'G3 h', 'A#3 h', 'D3 h', 'G3 h', 'A#3 h', 'C#3 h',
+    'F#3 h', 'A#3 h', 'D3 h', 'G3 h', 'A#3 h', 'C#3 h',
+    'D1 h', 'G1 h', 'A#2 h', 'D2 h', 'C#3 h',
   ],
-  
-  s1 : [
-    'C2 es',
-    'A#2 es',
-    'F2 h',
-    'G2 h',
-    'C2 q'
+
+  silence : [
+    'E1,A#2,F2 h s', 'E3 h', 'F3 h', 'D3 h',
+    'F3 h', 'E3 h', 'F3 h', 'D3 h',
+    'G2 q', 'A2 q', 'A#2 q', 'G2 q', 'C#3 h'
   ],
-  
-  s2 : [
-    'C1 q',
-    'C1 q',
-    'F2 q',
-    'G2 h',
-    'C2 q'
-  ],
-  
-  s3 : [
-    'B3 e h', 'F3 e h',   'A#3 e h',  'F#3 e h', 
-    'B3 e h', 'F3 e h',   'A#3 e h',  'F#3 e h',
-    'D3 q h', 'D#3 es h', 'A#2 es h',
-    'B3 e h', 'F3 e h',   'A#3 e h',  'F#3 e h', 
-    'B3 e h', 'F3 e h',   'A#3 e h',  'F#3 e h',
-    'D3 q h', 'D#3 es h', 'A#2 es h', 'F#2 q h',
-    'F2 q h', 'D2 e h',   'D#2 e h'
-  ],
-  
-  background1 : [
-    'B3 h s', '- h s',   'A#3 h s',  '- h s', 
-    'B3 e h', '- e',     'A#3 e',  'F#3 e h',
-    'D3 q h', 'D#3 es',   'A#2 es',
-    'B3 e h', 'F3 e',     'A#3 e',  'F#3 e h', 
-    'B3 e h', 'F3 e',     'A#3 e',  'F#3 e h',
-    'D3 q h', 'D#3 es',   'A#2 es', 'F#2 q h',
-    'F2 q h', 'D2 e',     'D#2 e'
+
+  proof : [
+    'D3 h', 'G3 h',  'D3 h', 'F#3 h',
+    'D2 h', 'D#2 h', 'C#2 h', 'D2 h',
+    'A1 h', 'A#1 h', 'G1 w'
   ]
 }
 
