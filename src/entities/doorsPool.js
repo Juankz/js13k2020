f8c7556ded4ad333154dc4b65546489662f69628
Juan Velandia <juankz@protonmail.com>
2020-09-13 03:15:41 -0500

    Make doors an obstacle

--- src/entities/doorsPool.js
+++ src/entities/doorsPool.js
@@ -3,6 +3,7 @@ import Pool from './pool.js';
 class DoorsPool extends Pool {
   createEntity() {
     const door = document.createElement('a-box');
+    door.classList.add('obstacles');
     door.setAttribute('texture-material', 'texture: door');
     door.setAttribute('height', 3);
     door.setAttribute('shadow', 'receive: true; cast: false;');
